047d272d8f70b3f163b440680c4b5d08f2d01467
rippleFCL <github@ripple.contact>
2025-07-23 17:06:21 +0100

    fix: wrong envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Create group if it doesn't exist
-if ! getent group "$SENSEHAT_GID" >/dev/null; then
-    groupadd -g "$SENSEHAT_GID" sensehat
+if ! getent group "$GID" >/dev/null; then
+    groupadd -g "$GID" sensehat
 fi
 
 # Create user if it doesn't exist
 if ! id -u sensehat >/dev/null 2>&1; then
-    useradd -m -u "$SENSEHAT_UID" -g "$SENSEHAT_GID" sensehat
+    useradd -m -u "$UID" -g "$GID" sensehat
 fi
 
 # Run main.py as sensehat user
